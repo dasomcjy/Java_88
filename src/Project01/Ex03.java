6462d61b9f117775839c18897b794361d87815c7
Choijiyoon <dasomcjy@naver.com>
2022-11-20 10:45:45 +0900

    11/20 커밋

--- src/Project01/Ex03.java
+++ src/Project01/Ex03.java
@@ -1,9 +1,75 @@
 package Project01;
 
+class Man {
+		
+	String name = "홍길동" ;
+	int age = 30;
+	String phone = "010-1111-1234";
+	String addr = "서울 종로구 인사동";
+	double weight = 70.5;
+	
+	//기본 생성자는 생략됨 : Man () {}
+	
+	//메소드 :
+	void add1 () {
+			//1~100까지2의 배수를 출력하고 더한값을 출력
+			int sum = 0;
+			for (int i = 1 ; i <=100; i++) {
+				
+				if (i % 2 == 0) {
+					sum+= i;
+					System.out.print(i + " ");
+				}
+			}
+			System.out.println();
+			System.out.print("2의 배수를 합한 값 : " + sum);
+	}
+	
+	void add2 () {
+		// 1~500 ,까지 3의 배수와 4의 배수를 출력하고 더한값 출력
+		int sum = 0;
+		for (int i = 1 ; i <= 500 ; i ++ ) {
+			if (i % 3 == 0 || i % 4 == 0) {
+				sum += i;
+				System.out.print(i + " ");
+
+			}
+		}
+		System.out.println();
+		System.out.print("3의 배수와 4의 배수와 합 : " + sum);
+	}
+	
+	void add3() {
+		//1~1000까지 1씩증가하고 4의 배수만 출력되지 않도록하고 더한값 출력
+		int sum = 0 ;
+		for (int i = 0 ; i <=1000; i++) {
+			if(i % 4 == 0) {
+				continue;
+			}
+			sum += i;
+			System.out.print( i + " ");
+		}
+		System.out.println();
+		System.out.print("모두 더한값 :" + sum);
+	}
+	
+}
+
 public class Ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 객체 생성 및 객체의 필드의 값이나 베소드 호출
+		
+		Man man = new Man ();
+		
+		man.add1();
+		
+		System.out.println("=========================================");
+		
+		man.add2();
+		
+		System.out.println("=========================================");
+		man.add3();
 
 	}
 
